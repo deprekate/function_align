d73dfeb154cefb472c6110286f43aa953af50f4d
deprekate <deprekate@gmail.com>
2021-01-13 17:01:24 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # function_align
 Code to multiple align a sets of ordered genes by their function 
+
+The workflow is divided into three steps:
+    1. encode gene names into letters
+	2. do multiple alignment with clustalw
+	3. decode the letters back into gene names
+
+
+Provided in this repo are several sample phage genomes in GenBank format and a namesfile with several common phage genes 
+
+
+## Quickstart
+```
+ls gb/* | xargs -i python3 encode.py {} names.tsv > encoded.fasta
+python3 align.py encoded.fasta
+python decode.py encoded_aligned.fasta
+```
+
+
+For the first step, use the script `encode_genbank.py` to take a genbank file and a namesfile.  The script parses the GenBank file looking for CDS *features* (protein-coding genes).  For each *feature* the script looks for one of the words from the namesfile (with priority given to words at the top of the namesfile) in every *qualifier* field of that *feature*.
+To run the script on one genoms you can use the following command:
+```
+encode.py gb/AB045978.gb names.tsv
+```
+One of the issues with aligning phage genomes is that many are circular so currently the script looks for an integrase and sets that as the beginning.  The other issue is in which direction (forward or reverse) to start adding to the gene order. Currently the script orders the direction by looking at the order of the collar and terminase genes
+
+
+The second step is to do a multiple sequence alighment of all the encoded genenames. This is done using clustalw and the provided custom protein weight matrix, where the weights for matches are 9, mismatches are -9, and wildcards (\*) are 0
+
+The specific clustalw command is 
+```
+#clustalw2 -INFILE=infile.fasta -OUTFILE=outfile.fasta -OUTPUT=fasta -MATRIX=MATRIX -TYPE=PROTEIN -GAPOPEN=0 -GAPEXT=0 -ALIGN -NEGATIVE
+```
+
---
+++ encode.py
@@ -0,0 +1,64 @@
+import os
+import re
+import sys
+from pathlib import Path
+import textwrap
+import argparse
+from argparse import RawTextHelpFormatter
+from itertools import groupby
+
+import Bio
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
+
+
+def is_valid_file(x):
+    if not os.path.exists(x):
+        raise argparse.ArgumentTypeError("{0} does not exist".format(x))
+    return x
+
+def get_args():
+    usage = 'encode_genbank.py [-opt1, [-opt2, ...]] infile'
+    parser = argparse.ArgumentParser(description='encode_genbank', formatter_class=RawTextHelpFormatter, usage=usage)
+    parser.add_argument('infile', type=is_valid_file, help='input file in fasta format')
+    parser.add_argument('namefile', type=is_valid_file, help='names file with one name per line')
+    parser.add_argument('-o', '--outfile', action="store", default=sys.stdout, type=argparse.FileType('w'), help='where to write the output [stdout]')
+    parser.add_argument('-s', '--squeeze', action="store_true", help='squeeze identical consecutive names into one letter')
+    parser.add_argument('-i', '--ignore', action="store_false", help='ignore genes that do not match any of the names')
+    return parser.parse_args()
+args = get_args()
+
+
+names = dict()
+with open(args.namefile) as fp:
+	for line,letter in zip(fp, 'ARHDCQEGNILKMFPSTWYVBOZX'):
+		names[line.rstrip().lower()] = letter
+
+#sys.stderr.write(str(names) + "\n")
+
+infile = open(args.infile, 'r')
+for record in SeqIO.parse(infile, "genbank") :
+	letters = []
+	for feature in record.features :
+		if feature.type=="CDS" :
+			letter = '*' if args.ignore else ''
+			values = " ".join(map(str,feature.qualifiers.values())).lower()
+			for name in names:
+				if re.search(rf"\b{name}\b", values, re.IGNORECASE):
+					letter = names[name]
+					break
+			letters.append(letter)
+
+	print(">" + Path(args.infile).stem)
+	# recircularize the order so that integrase is first
+	index = letters.index(names['integrase'])
+	if letters.index(names['terminase']) < letters.index(names['portal']):
+		letters = letters[index:] + letters[:index]
+	else:
+		letters = list(reversed(letters[:index+1])) + list(reversed(letters[index+1:]))
+	if args.squeeze:
+		letters = [i[0] for i in groupby(letters)]
+	print("".join(letters))
+					
+
